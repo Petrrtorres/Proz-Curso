5cf8d259070b9a885d68537837dc476a1b297181
Peterson Torres <devpetrr@gmail.com>
2023-12-21 16:51:02 -0300

    Update calculadora.py

--- funcao/calculadora.py
+++ funcao/calculadora.py
@@ -1,19 +1,21 @@
-def calculadora (num1, operacao, num2):
-      num1 =0
-      num2 = 0
-      operacao = ' '
-
-      if operacao == '1':
+def calculadora (num1, operacao, num2): 
+      if operacao == 1:
         resultado=num1+num2
-      elif operacao == '2':
+      elif operacao == 2:
         resultado=num1-num2
-      elif operacao == '3':
+      elif operacao == 3:
         resultado=num1*num2
-      elif operacao == '4':
+      elif operacao == 4:
         resultado=num1/num2
       else:
         resultado=0
         print("Operação inexistente, o resultado é: " + str(resultado))
 
+      return resultado
 
-calculadora (3, '4', 8)
+num1 = int(input("Digite um número: "))
+operacao = int(input("Digite uma operação: "))
+num2 = int(input("Digite o segundo número: "))     
+
+resultado = calculadora(num1,operacao,num2)
+print(resultado)

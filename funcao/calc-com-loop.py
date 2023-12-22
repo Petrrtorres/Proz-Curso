d31efe289b21cc6e720e553e41edf998b4a5237c
Peterson Torres <devpetrr@gmail.com>
2023-12-22 17:11:32 -0300

    Update calc-com-loop.py

--- funcao/calc-com-loop.py
+++ funcao/calc-com-loop.py
@@ -17,8 +17,7 @@ def calculadora (operacao, num1, num2):
 
 
 #Programa principal
-    
-i= True
+i=None
 
 while i != 0:
 
@@ -26,9 +25,13 @@ while i != 0:
   print(" 1: Soma \n 2: Subtração \n 3: Multiplicação \n 4: Divisão \n 0: Sair \n")
 
   operacao = int(input("Digite o número da sua operação, ou [0] para sair: "))
-  num1 = int(input("Digite o primeiro número: "))
-  num2 = int(input("Digite o segundo número: "))
+  if operacao == 0 or operacao < 0 or operacao >= 5:
+      i = 0
+      print("Fim da operação.")
+  else: 
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
 
-  resposta = calculadora(operacao, num1, num2)
-  print("\n O resultado é: " + str(resposta) + '\n')
-  print('--' *30)
+    resposta = calculadora(operacao, num1, num2)
+    print("\n O resultado é: " + str(resposta) + '\n')
+    print('--' *30)

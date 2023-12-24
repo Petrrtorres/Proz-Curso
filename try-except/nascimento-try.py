1c099f8a330c5cab22616833d97da2985c4c50e8
Peterson Torres <devpetrr@gmail.com>
2023-12-24 16:12:41 -0300

    Update nascimento-try.py

--- try-except/nascimento-try.py
+++ try-except/nascimento-try.py
@@ -1,14 +1,12 @@
-try:
-      nome_completo = str(input("Digite seu nome completo: "))
-      ano_nascimento = int(input("Digite seu ano de nascimento: "))
-      
-      if ano_nascimento < 1922 and ano_nascimento > 2021:
-         idade = 2022 - ano_nascimento 
-      else:
-         print('Ano de nascimento inválido. \n' )
+nome = str(input("Digite seu nome completo: "))
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
 
-except Exception as erro: 
-      print(erro.__cause__)
-
-else:
-      print(f'Seu nome é {nome_completo.upper()} e sua idade é {idade}')
+i=True
+while i != False:
+      try:
+            if ano_nascimento > 1922 and ano_nascimento < 2021:
+                  idade = 2022 - ano_nascimento
+                  print(f'Seu nome é {nome.upper()} e você tem {idade} anos.')
+                  i = False
+      except Exception as erro:
+            print(str('Dados inválidos: ' + erro.__cause__))

c21e39d8bd364611308b9e1a618a4f469f928784
Peterson Torres <devpetrr@gmail.com>
2023-12-24 16:34:23 -0300

    Update nascimento-try.py

--- try-except/nascimento-try.py
+++ try-except/nascimento-try.py
@@ -1,10 +1,13 @@
 nome = str(input("Digite seu nome completo: "))
-ano_nascimento = int(input("Digite seu ano de nascimento: "))
 
 i=True
+
 while i != False:
+      ano_nascimento=int(input("Digite seu ano de nascimento: "))
       try:
-            if ano_nascimento > 1922 and ano_nascimento < 2021:
+            if ano_nascimento < 1922 or ano_nascimento > 2021:
+                  print('O ano informado precisa ser entre 1922 e 2021.')
+            else:
                   idade = 2022 - ano_nascimento
                   print(f'Seu nome é {nome.upper()} e você tem {idade} anos.')
                   i = False

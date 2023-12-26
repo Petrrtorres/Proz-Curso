0d43ffeb3beeb22aa9a4aa40e8be59432847369b
Peterson Torres <devpetrr@gmail.com>
2023-12-26 15:08:58 -0300

    Create arrays4.py

---
+++ arrays/arrays4.py
@@ -0,0 +1,9 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+print(lista_produtos) 
+
+lista_produtos[1], lista_produtos[4] = 'rímel', 'hidratantes'
+print(lista_produtos)
+
+lista_produtos.append('acetona')
+lista_produtos.append('algodão')
+print(lista_produtos)
